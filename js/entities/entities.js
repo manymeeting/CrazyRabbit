32639db2fbdc401e727c0ab2db26b2528fde94c3
siriustom <linxiaoran0210@gmail.com>
2017-09-29 16:17:27 -0700

    diablo_8

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -89,8 +89,170 @@ game.PlayerEntity = me.Entity.extend({
      * colision handler
      * (called when colliding with other objects)
      */
+     onCollision : function (response, other) {
+         switch (response.b.body.collisionType) {
+             case me.collision.types.WORLD_SHAPE:
+             // Simulate a platform object
+             if (other.type === "platform") {
+                 if (this.body.falling &&
+                     !me.input.isKeyPressed('down') &&
+                     
+                     // Shortest overlap would move the player upward
+                     (response.overlapV.y > 0) &&
+                     
+                     // The velocity is reasonably fast enough to have penetrated to the overlap depth
+                     (~~this.body.vel.y >= ~~response.overlapV.y)
+                     ) {
+                         // Disable collision on the x axis
+                         response.overlapV.x = 0;
+                         
+                         // Repond to the platform (it is solid)
+                         return true;
+                       }
+             
+                 // Do not respond to the platform (pass through)
+                 return false;
+             }
+             break;
+         
+             case me.collision.types.ENEMY_OBJECT:
+             if ((response.overlapV.y>0) && !this.body.jumping) {
+                 // bounce (force jump)
+                 this.body.falling = false;
+                 this.body.vel.y = -this.body.maxVel.y * me.timer.tick;
+                 
+                 // set the jumping flag
+                 this.body.jumping = true;
+             }
+             else {
+                 // let's flicker in case we touched an enemy
+                 this.renderable.flicker(750);
+             }
+             
+             // Fall through
+             
+             default:
+                 // Do not respond to other objects (e.g. coins)
+                 return false;
+             }
+         
+         // Make the object solid
+         return true;
+     }
+});
+/**
+ * a Coin entity
+ */
+game.CoinEntity = me.CollectableEntity.extend({
+    // extending the init function is not mandatory
+    // unless you need to add some extra initialization
+    init: function (x, y, settings) {
+        // call the parent constructor
+        this._super(me.CollectableEntity, 'init', [x, y , settings]);
+    },
+
+    // this function is called by the engine, when
+    // an object is touched by something (here collected)
     onCollision : function (response, other) {
+        // do something when collected
+
+        // make sure it cannot be collected "again"
+        this.body.setCollisionMask(me.collision.types.NO_OBJECT);
+
+        // remove it
+        me.game.world.removeChild(this);
+
+        return false
+    }
+});
+
+/**
+ * an enemy Entity
+ */
+game.EnemyEntity = me.Entity.extend({
+    init: function (x, y, settings) {
+        // define this here instead of tiled
+        settings.image = "wheelie_right";
+
+        // save the area size defined in Tiled
+        var width = settings.width;
+        var height = settings.height;
+
+        // adjust the size setting information to match the sprite size
+        // so that the entity object is created with the right size
+        settings.framewidth = settings.width = 64;
+        settings.frameheight = settings.height = 64;
+
+        // redefine the default shape (used to define path) with a shape matching the renderable
+        settings.shapes[0] = new me.Rect(0, 0, settings.framewidth, settings.frameheight);
+
+        // call the parent constructor
+        this._super(me.Entity, 'init', [x, y , settings]);
+
+        // set start/end position based on the initial area size
+        x = this.pos.x;
+        this.startX = x;
+        this.endX   = x + width - settings.framewidth
+        this.pos.x  = x + width - settings.framewidth;
+
+        // to remember which side we were walking
+        this.walkLeft = false;
+
+        // walking & jumping speed
+        this.body.setVelocity(4, 6);
+
+    },
+
+    /**
+    * update the enemy pos
+    */
+    update : function (dt) {
+
+        if (this.alive) {
+        if (this.walkLeft && this.pos.x <= this.startX) {
+            is.walkLeft = false;
+        }
+        else if (!this.walkLeft && this.pos.x >= this.endX) {
+            is.walkLeft = true;
+        }
+
+        // make it walk
+        this.renderable.flipX(this.walkLeft);
+        this.body.vel.x += (this.walkLeft) ? -this.body.accel.x * me.timer.tick : this.body.accel.x * me.timer.tick;
+        }
+        else {
+            is.body.vel.x = 0;
+        }
+
+        // update the body movement
+        this.body.update(dt);
+
+        // handle collisions against other shapes
+        me.collision.check(this);
+
+        // return true if we moved or if the renderable was updated
+        return (this._super(me.Entity, 'update', [dt]) || this.body.vel.x !== 0 || this.body.vel.y !== 0);
+    },
+
+    /**
+    * colision handler
+    * (called when colliding with other objects)
+    */
+    onCollision : function (response, other) {
+        if (response.b.body.collisionType !== me.collision.types.WORLD_SHAPE) {
+        // res.y >0 means touched by something on the bottom
+        // which mean at top position for this one
+            if (this.alive && (response.overlapV.y > 0) && response.a.body.falling) {
+                s.renderable.flicker(750);
+            }
+            return false;
+        }
         // Make all other objects solid
         return true;
     }
 });
+
+// register our object entities in the object pool
+me.pool.register("mainPlayer", game.PlayerEntity);
+me.pool.register("CoinEntity", game.CoinEntity);
+me.pool.register("EnemyEntity", game.EnemyEntity);
