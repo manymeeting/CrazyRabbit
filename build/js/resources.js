f6fe30252402425e7d795ba53cfdc844adcbd4c8
Adamkzh <kzh88@hotmail.com>
2017-11-01 20:14:12 -0700

    add HUB 😊

--- build/js/resources.js
+++ build/js/resources.js
@@ -73,5 +73,13 @@ game.resources = [
         "name": "scroll",
         "type": "tmx",
         "src": "data/map/scroll.tmx"
-    }
+    },
+    { name: "PressStart2P",
+      type:"image",
+      src: "data/fnt/PressStart2P.png"
+    },  
+    { name: "PressStart2P",
+     type:"binary",
+     src: "data/fnt/PressStart2P.fnt"
+    },
 ];
