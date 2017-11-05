4aebf185a0063809a2f9c5f6cd0000d8f2a518f8
Mokuten OH <mokuteno@gmail.com>
2017-11-05 15:51:04 -0800

    add new tileset image

---
+++ data/map/area02_level_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="area02_level_tiles" tilewidth="32" tileheight="32" tilecount="768" columns="32">
+ <image source="../img/map/area02_level_tiles.png" width="1024" height="768"/>
+</tileset>
