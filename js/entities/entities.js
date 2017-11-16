27c1e352dd52057458aa92e4e9a54b1467cb0bdc
Ruifeng Sheng <ruifengsheng@gmail.com>
2017-11-15 21:45:18 -0800

    semicolumn

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -221,7 +221,7 @@ game.EnemyEntity = me.Entity.extend({
         // set start/end position based on the initial area size
         x = this.pos.x;
         this.startX = x;
-        this.endX = x + width - settings.framewidth
+        this.endX = x + width - settings.framewidth;
         this.pos.x = x + width - settings.framewidth;
 
         // to remember which side we were walking
