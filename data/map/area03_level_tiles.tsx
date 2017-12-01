a0e59507d15062d6cdf0c7dce2a2827e3216a4aa
Mokuten OH <mokuteno@gmail.com>
2017-12-01 14:01:08 -0800

    add tileset for area03

---
+++ data/map/area03_level_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="area03_level_tiles" tilewidth="32" tileheight="32" tilecount="98" columns="14">
+ <image source="../img/map/area03_level_tiles.png" width="448" height="224"/>
+</tileset>
