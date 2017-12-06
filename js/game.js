e39b63272b20a0f7626f1d7c93cb0127780f0a24
siriustom <linxiaoran0210@gmail.com>
2017-12-05 19:57:16 -0800

    debug on 2nd level

--- js/game.js
+++ js/game.js
@@ -12,17 +12,31 @@ var game = {
     // Run on page load.
     "onload" : function () {
         // Initialize the video.
-        if (!me.video.init(640, 480, {wrapper : "screen", scale : "auto"})) {
+        if (!me.video.init(640, 480, {wrapper : "screen", scale : "auto", scaleMethod: "flex-width"})) {
             alert("Your browser does not support HTML5 canvas.");
             return;
+        }
+
+        // add "#debug" to the URL to enable the debug Panel
+        if (me.game.HASH.debug === true) {
+            window.onReady(function () {
+                me.plugin.register.defer(this, me.debug.Panel, "debug", me.input.KEY.V);
+            });
         }
 
         // Initialize the audio.
         me.audio.init("mp3,ogg");
 
-        // set and load all resources.
-        // (this will also automatically switch to the loading screen)
-        me.loader.preload(game.resources, this.loaded.bind(this));
+        // Set a callback to run when loading is complete.
+        me.loader.onload = this.loaded.bind(this);
+
+        // Load the resources.
+        me.loader.preload(game.resources);
+
+        // Initialize melonJS and display a loading screen.
+        me.state.change(me.state.LOADING);
+        // // (this will also automatically switch to the loading screen)
+        // me.loader.preload(game.resources, this.loaded.bind(this));
     },
 
     // Run on game resources loaded.
