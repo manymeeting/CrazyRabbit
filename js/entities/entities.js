dabea8adc1ecaddd39e22c001628ead8c78b986b
Adamkzh <kzh88@hotmail.com>
2017-12-06 17:23:11 -0800

    Some Try

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -451,7 +451,7 @@ game.FallingBlockEntity = me.Entity.extend({
 game.FloatingEnemyEntity0 = me.Entity.extend({
     init: function(x, y, settings) {
         // define this here instead of tiled
-        settings.image = "Flying enemy";
+        settings.image = "snail";
 
         // save the area size defined in Tiled
         var width = settings.width;
@@ -474,17 +474,41 @@ game.FloatingEnemyEntity0 = me.Entity.extend({
         // set start/end position based on the initial area size
         x = this.pos.x;
         this.startX = x;
-        this.endX = x;
-        this.pos.x = x;
+        this.endX = x + width - settings.framewidth;
+        this.pos.x = x + width - settings.framewidth;
 
         // to remember which side we were walking
         this.walkLeft = false;
 
         // walking & jumping speed
-        this.body.setVelocity(4, 6);
+        this.body.setVelocity(5, 12);
 
     },
+    update: function(dt) {
 
+        if (this.alive) {
+            if (this.walkLeft && this.pos.x <= this.startX) {
+                this.walkLeft = false;
+            } else if (!this.walkLeft && this.pos.x >= this.endX) {
+                this.walkLeft = true;
+            }
+
+            // make it walk
+            this.renderable.flipX(this.walkLeft);
+            this.body.vel.x += (this.walkLeft) ? -this.body.accel.x * me.、timer.tick : this.body.accel.x * me.timer.tick;
+        } else {
+            this.body.vel.x = 0;
+        }
+
+        // update the body movement
+        this.body.update(dt);
+
+        // handle collisions against other shapes
+        me.collision.check(this);
+
+        // return true if we moved or if the renderable was updated
+        return (this._super(me.Entity, 'update', [dt]) || this.body.vel.x !== 0 || this.body.vel.y !== 0);
+    },
     onCollision: function(response, other) {
         if (response.b.body.collisionType !== me.collision.types.WORLD_SHAPE) {
             // res.y > 0 means touched by something on the bottom
@@ -510,4 +534,18 @@ game.FloatingEnemyEntity0 = me.Entity.extend({
         // make all other objects solid
         return true;
     }
+});
+
+
+/**
+ * Life point of the enemy
+ * @type Number
+ * @name lifePoint
+ * @memberOf game.EnemyEntity
+ */
+Object.defineProperty(game.FloatingEnemyEntity0.prototype, "lifePoint", {
+    enumerable: true,
+    configurable: false,
+    writable: true,
+    value: 2
 });
