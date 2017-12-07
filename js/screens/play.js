f51c2d7c5fc611a5e2e1344f1ac449c5d1fe47b4
Mokuten OH <mokuteno@gmail.com>
2017-12-07 01:12:13 -0800

    begin from level2 for convenience

--- js/screens/play.js
+++ js/screens/play.js
@@ -5,7 +5,7 @@ game.PlayScreen = me.ScreenObject.extend({
     onResetEvent: function() {
         me.audio.playTrack("bgm2");
         // load a level
-        me.levelDirector.loadLevel("area02");
+        me.levelDirector.loadLevel("area03");
 
         // reset the score
         game.data.score = 0;
